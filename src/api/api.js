3f5ff8d369b50ea1252b28ea8aa14a083e3de2f8
wd <1065274258@qq.com>
2022-09-24 19:43:06 +0800

    增加3d接口

--- src/api/api.js
+++ src/api/api.js
@@ -42,6 +42,12 @@ export let getFurtherCrop = (base64, isonserver) => {
     }
     return axios.post(serverUrl + '/draft/furtherCrop', qs.stringify({value: base64, base64, isonserver, scale: 1}));
 };
+export let getFurtherCrop3D = (base64, isonserver) => {
+    if (/data:image/.test(base64)) {
+        base64 = base64.split(',')[1];
+    }
+    return axios.post(serverUrl + '/draft/furtherCrop3D', qs.stringify({value: base64, base64, isonserver, scale: 1}));
+};
 
 /**
  *
