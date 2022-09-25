c5c2f76de196dd11beac10099cfa9883bd96c4e4
wd <1065274258@qq.com>
2022-09-25 16:16:56 +0800

    tijiao

--- src/api/api.js
+++ src/api/api.js
@@ -46,6 +46,7 @@ export let getFurtherCrop3d = (base64, isonserver) => {
     if (/data:image/.test(base64)) {
         base64 = base64.split(',')[1];
     }
+    console.log(123)
     return axios.post(serverUrl + '/draft/3Ddraft' ,qs.stringify({ base64, isonserver}))
 };
 
